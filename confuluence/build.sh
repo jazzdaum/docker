5d38775b7363171409c6d74164c03040f8a0ce2b
zhyr <2205549788@qq.com>
2020-04-08 16:59:48 +0800

    修改文件

--- confuluence/build.sh
+++ confuluence/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker rmi dockerhub-cn-north-1.jcr.service.jdcloud.com/atlassian-confluence:v7.3.4-2
-sudo docker build -t dockerhub-cn-north-1.jcr.service.jdcloud.com/atlassian-confluence:v7.3.4-2 .
+sudo docker rmi xxx.com/atlassian-confluence:v7.3.4
+sudo docker build -t xxx.com/atlassian-confluence:v7.3.4 .
